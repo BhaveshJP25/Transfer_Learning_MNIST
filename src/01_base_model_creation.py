574736e9faf1cd717b4dfdf740afd40e5d28af35
BhaveshJP25 <bhaveshparakh25@gmail.com>
2021-12-01 18:15:27 +0530

    01 Base Model Completed

--- src/01_base_model_creation.py
+++ src/01_base_model_creation.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
 import tensorflow as tf
 
 STAGE = "Creating Base Model" ## <<< change stage name 
@@ -37,7 +37,29 @@ def main(config_path):
     OPTIMIZER = config["params"]["optimizer"]
     NUM_CLASSES = config["params"]["num_classes"]
 
+    #Model Creation
     model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = (X_valid, y_valid)
+    
+    #Model training
+    history = model.fit(
+        X_train, y_train, 
+        epochs=EPOCHS, 
+        validation_data=VALIDATION_SET,
+        verbose=1)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+    model_name = config["artifacts"]["base_model_name"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    save_model(model, model_name, model_dir_path)
+    logging.info(f"Evaluation Metrics : {model.evaluate(X_test, y_test)}")
+    
 
 
 if __name__ == '__main__':
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
 import os
 import time
-import pandas as pd
+import logging
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
@@ -14,6 +21,7 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
 
     model_clf = tf.keras.models.Sequential(LAYERS)
 
+    # logging.info(f"Model Summary : \n {model_clf.summary()}")
     model_clf.summary()
 
     model_clf.compile(loss=LOSS_FUNCTION,
@@ -21,3 +29,14 @@ def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
                 metrics=METRICS)
 
     return model_clf ## <<< untrained model
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
+    logging.info(f"Base Model is Saved at {path_to_model}")
